200bec94d554196b794c9cadf3643acd3f36211b
yinbinyu77877 <yubin.yin@dingdandao.com>
2022-11-14 17:52:04 +0800

    feat: 提交

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,16 @@
 module.exports = {
-  plugins: {
-    'tailwindcss/nesting': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+	plugins: {
+		'tailwindcss/nesting': {},
+		tailwindcss: {},
+		autoprefixer: {
+			overrideBrowserslist: [
+				'Android 4.1',
+				'iOS 7.1',
+				'Chrome > 31',
+				'ff > 31',
+				'ie >= 8',
+				'last 10 versions' // 所有主流浏览器最近10版本用
+			]
+		}
+	}
+};
